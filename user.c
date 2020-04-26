3854c33ce052904bafd53e32ce5fa970e91f6c2f
Q <50916196+QHman@users.noreply.github.com>
2020-04-26 17:57:05 -0400

    Update user.c

--- user.c
+++ user.c
@@ -2,8 +2,8 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-char proc_buffer[1000];
-int proc_buffer_size = 0
+char proc_buffer[1000] = "Hello";
+size_t proc_buffer_size = 5;
 
 static int proc_show(struct seq_file *m, void *v) {
   seq_printf(m, "Hello proc!\n");
@@ -14,7 +14,7 @@ static int proc_open(struct inode *inode, struct  file *file) {
   return single_open(file, proc_show, NULL);
 }
 
-int proc_read(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data){
+int proc_read(struct file *filp, char *buffer, size_t buff_length){
 	int len;
 
 	if (offset > 0) {
@@ -23,12 +23,13 @@ int proc_read(char *buffer, char **buffer_location, off_t offset, int buffer_len
 	} else {
 		/* fill the buffer, return the buffer size */
 		memcpy(buffer, proc_buffer, proc_buffer_size);
+
 		len = procfs_buffer_size;
 	}
 
 	return ret;
 }
-static const struct file_operations hello_proc_fops = {
+static const struct file_operations proc_fops = {
   .owner = THIS_MODULE,
   .open = proc_open,
   .read = proc_read,
@@ -36,7 +37,9 @@ static const struct file_operations hello_proc_fops = {
 };
 
 static int __init proc_init(void) {
-  proc_create("hello_proc", 0, NULL, &hello_proc_fops);
+  proc_create("hello_proc", 0, NULL, &hproc_fops);
+	proc_buffer[proc_buffer_size] = '1';
+	proc_buffer_size += 1;
   return 0;
 }
 
