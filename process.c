4ee03accac4e159ce010bd343286237dae454141
Q <50916196+QHman@users.noreply.github.com>
2020-04-26 18:29:17 -0400

    Update process.c

--- process.c
+++ process.c
@@ -1,114 +1,42 @@
-/**
- *  procfs2.c -  create a "file" in /proc
- *
- */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/unistd.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <linux/proc_fs.h>
 
-#include <linux/module.h>	/* Specifically, a module */
-#include <linux/kernel.h>	/* We're doing kernel work */
-#include <linux/proc_fs.h>	/* Necessary because we use the proc fs */
-#include <asm/uaccess.h>	/* for copy_from_user */
+struct proc_dir_entry *tlc_proc_a;
+char proc_buffer[100] = "Hello";
+int proc_buffer_size = 5;
 
-#define PROCFS_MAX_SIZE		1024
-#define PROCFS_NAME 		"buffer1k"
-
-/**
- * This structure hold information about the /proc file
- *
- */
-static struct proc_dir_entry *Our_Proc_File;
-
-/**
- * The buffer used to store character for this module
- *
- */
-static char procfs_buffer[PROCFS_MAX_SIZE];
-
-/**
- * The size of the buffer
- *
- */
-static unsigned long procfs_buffer_size = 0;
-
-/**
- * This function is called then the /proc file is read
- *
- */
-int
-procfile_read(char *buffer,
-	      char **buffer_location,
-	      off_t offset, int buffer_length, int *eof, void *data)
+static ssize_t proc_read(struct file *fp, char *buf, size_t len, loff_t * off)
 {
-	int ret;
+  static int finished=0;
+  if(finished) {
+    finished = 0;
+    return 0;
+  }
+  finished=1;
+		memcpy(buffer, proc_buffer, proc_buffer_size);
 
-	printk(KERN_INFO "procfile_read (/proc/%s) called\n", PROCFS_NAME);
-
-	if (offset > 0) {
-		/* we have finished to read, return 0 */
-		ret  = 0;
-	} else {
-		/* fill the buffer, return the buffer size */
-		memcpy(buffer, procfs_buffer, procfs_buffer_size);
-		ret = procfs_buffer_size;
-	}
-
-	return ret;
+	return proc_buffer_size;
 }
 
-/**
- * This function is called with the /proc file is written
- *
- */
-int procfile_write(struct file *file, const char *buffer, unsigned long count,
-		   void *data)
+static struct file_operations proc_fops = {
+  .owner= THIS_MODULE,
+  .read= proc_read};
+
+static int hello_init(void)
 {
-	/* get buffer size */
-	procfs_buffer_size = count;
-	if (procfs_buffer_size > PROCFS_MAX_SIZE ) {
-		procfs_buffer_size = PROCFS_MAX_SIZE;
-	}
 
-	/* write data to the buffer */
-	if ( copy_from_user(procfs_buffer, buffer, procfs_buffer_size) ) {
-		return -EFAULT;
-	}
-
-	return procfs_buffer_size;
+  proc_create("tlc_proc_a", 0444, NULL, &proc_fops);
+  return 0;
 }
 
-/**
- *This function is called when the module is loaded
- *
- */
-int init_module()
-{
-	/* create the /proc file */
-	proc_create(PROCFS_NAME, 0, NULL, Our_Proc_File);
-
-	if (Our_Proc_File == NULL) {
-		remove_proc_entry(PROCFS_NAME, &proc_root);
-		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",
-			PROCFS_NAME);
-		return -ENOMEM;
-	}
-
-	Our_Proc_File->read_proc  = procfile_read;
-	Our_Proc_File->write_proc = procfile_write;
-	Our_Proc_File->owner 	  = THIS_MODULE;
-	Our_Proc_File->mode 	  = S_IFREG | S_IRUGO;
-	Our_Proc_File->uid 	  = 0;
-	Our_Proc_File->gid 	  = 0;
-	Our_Proc_File->size 	  = 37;
-
-	printk(KERN_INFO "/proc/%s created\n", PROCFS_NAME);
-	return 0;	/* everything is ok */
+static void hello_exit(void)
+{  remove_proc_entry("tlc_proc_a", NULL);
 }
 
-/**
- *This function is called when the module is unloaded
- *
- */
-void cleanup_module()
-{
-	remove_proc_entry(PROCFS_NAME, &proc_root);
-	printk(KERN_INFO "/proc/%s removed\n", PROCFS_NAME);
-}
+module_init(hello_init);
+module_exit(hello_exit);
