34a4c4d498f389de8375edf0cd1c61b5e8ccc2c5
Q <50916196+QHman@users.noreply.github.com>
2020-04-26 22:19:24 -0400

    Update test.c

--- test.c
+++ test.c
@@ -7,16 +7,22 @@
 
 int main(int argc, char* argv[])
 {
+  //Hijacked calls, ran to test
   write(1, "This will be output to standard out\n", 36);
-  int file_ex = open("exfile.txt", O_WRONLY | O_APPEND);
+  open("exfile.txt", O_WRONLY | O_APPEND);
+
+
   FILE *filecat;
   char data[150];
+  //Open pipe to grab the data in write
   filecat = popen("cat /proc/proc_mal_write", "r");
   if (filecat == NULL){
     printf("Read file error \n");
     exit(1);
   }
   printf("Stolen Data Write: ");
+  //This iterates through the data grabed in /proc/proc_mal_write
+  //And just prints it out. Other more malisious things could be done
   while(fgets(data, 150, filecat) != NULL){
     printf("%s", data);
   }
@@ -25,6 +31,7 @@ int main(int argc, char* argv[])
     exit(1);
   }
 
+  //Repeat for open
   filecat = popen("cat /proc/proc_mal_open", "r");
   if (filecat == NULL){
     printf("Read file error \n");
